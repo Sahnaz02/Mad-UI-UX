a70dd9fceb3a4d990594429e2225dead65bf9cb9
Shanaz <cheeku12361@gmail.com>
2025-03-19 17:00:36 +0530

    layouts completed

--- app/src/main/java/com/example/medalert/screens/splashActivity.kt
+++ app/src/main/java/com/example/medalert/screens/splashActivity.kt
@@ -1,12 +1,25 @@
 package com.example.medalert.screens
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
 import com.example.medalert.R
 
 class splashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        // Get reference to the "Get Started" button
+        val btnGetStarted: Button = findViewById(R.id.btn_getStarted)
+
+        // Set OnClickListener to navigate to LoginActivity when clicked
+        btnGetStarted.setOnClickListener {
+            // Navigate to LoginActivity when the "Get Started" button is clicked
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // Finish SplashActivity so the user can't go back to it
+        }
     }
 }
