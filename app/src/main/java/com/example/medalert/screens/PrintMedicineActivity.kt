107956f716c16a08cd0c7e36252cc3f8fb8fe477
Shanaz <cheeku12361@gmail.com>
2025-03-21 18:59:15 +0530

    PrintMedications

---
+++ app/src/main/java/com/example/medalert/screens/PrintMedicineActivity.kt
@@ -0,0 +1,11 @@
+package com.example.medalert.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrintMedicineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_print_medicine)
+    }
+}
