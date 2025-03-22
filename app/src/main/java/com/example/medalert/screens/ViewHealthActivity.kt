f5546af7cab0e1d75938f6644beee33de05c7f70
Shanaz <cheeku12361@gmail.com>
2025-03-22 14:23:58 +0530

    complete

---
+++ app/src/main/java/com/example/medalert/screens/ViewHealthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.medalert.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.medalert.R
+
+class ViewHealthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_health)
+    }
+}
