d656dba9fb988be19e15e3d8019b5e142caa8cab
Shanaz <cheeku12361@gmail.com>
2025-03-22 14:40:37 +0530

    create profile

---
+++ app/src/main/java/com/example/medalert/screens/CreateProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.medalert.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.medalert.R
+
+class CreateProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_profile)
+    }
+}
